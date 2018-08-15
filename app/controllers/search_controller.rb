c76c2d4326da8ba13613795732cbc879bdbcbe32
Zach Bro <zachbroomy@hotmail.com>
2018-08-15 16:38:14 +1000

    Various bugfixes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,7 @@ class SearchController < ApplicationController
     if params[:search_for_ticket]
       @ticket = Ticket.find_by number: "#{params[:search_for_ticket]}"
     elsif params[:search_for_name]
-      first_ticket = Ticket.where("name like ?", "#{params[:search_for_name].upcase}%")
-      @ticket = first_ticket.where(active: true)
+      @tickets = Ticket.where("name like ?", "#{params[:search_for_name].upcase}%").where(active: true)
     end
   end
 end
