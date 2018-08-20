f27c18a7360abb48a1f58dffdf5fd976b5710e54
Zach Bro <zachbroomy@hotmail.com>
2018-08-20 22:14:43 +1000

    Fix several validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ Ticket.create!(number: "300000", name: "Smith. J", details_attributes:
            {amount: "4", location: "1C", room: "1010", aasm_state: "ST", s_employee_id: "300003"}])
 
 Ticket.create!(number: "300001", name: "Nguyen. K", details_attributes:
-          [{amount: "7", location: "5C", room: "1710", aasm_state: "ST", s_employee_id: "300002"},
-           {amount: "1", location: "1A", room: "1010", aasm_state: "ST", s_employee_id: "300001"}])
+          [{amount: "1", location: "1A", room: "1010", aasm_state: "ST", s_employee_id: "300001"},
+           {amount: "7", location: "3C Wall", room: "1710", aasm_state: "ST", s_employee_id: "300002"}])
 
 Ticket.create!(number: "300002", name: "O'Brien. T", active: false, details_attributes:
           [{amount: "1", location: "2B", room: "1515", aasm_state: "ST", s_employee_id: "300000",
@@ -23,7 +23,7 @@ Ticket.create!(number: "300002", name: "O'Brien. T", active: false, details_attr
 
 Ticket.create!(number: "300003", name: "Zhao. Y", details_attributes:
           [{amount: "4", location: "4C", room: "1010", aasm_state: "RNR", s_employee_id: "300003"},
-           {amount: "3", location: "2A", room: "1810", aasm_state: "ST", s_employee_id: "300002"}])
+           {amount: "3", location: "2A + 1 in fridge", room: "1810", aasm_state: "ST", s_employee_id: "300002"}])
 
 Ticket.create!(number: "300005", name: "Jones. S", details_attributes:
           [{amount: "1", location: "3A", room: "1710", aasm_state: "ST", s_employee_id: "300004"},
