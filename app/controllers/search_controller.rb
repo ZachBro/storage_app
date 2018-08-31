1e7b993e2a48766b0d04083fcb6f03c121e98c66
Zach Bro <zachbroomy@hotmail.com>
2018-08-31 11:59:41 +1000

    Start advanced search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,25 @@
 class SearchController < ApplicationController
   def index
-    if params[:ticket]
-      @ticket = Ticket.find_by number: "#{params[:ticket]}"
-    elsif params[:name_date] && params[:date]
-      date_var = params[:date]
-      search_date = Date.new(*flatten_date_array(date_var)).in_time_zone("Melbourne").at_beginning_of_day
-      @tickets = Ticket.where("name like ?", "#{params[:name_date].upcase}%").where("updated_at > ?", search_date).or(
-                 Ticket.where("name like ?", "#{params[:name_date].upcase}%").where(active: true))
+    if params[:name].present? && params[:room].present?
+      date_start = params[:ds]
+      date_end = params[:de]
+      search_start = Date.new(*flatten_date_array(date_start, "ds")).
+        in_time_zone("Melbourne").at_beginning_of_day
+      search_end = Date.new(*flatten_date_array(date_end, "de")).
+        in_time_zone("Melbourne").at_end_of_day
+      @tickets = Ticket.joins(:details).where("room = ?", "#{params[:room]}").
+        where("name like ?", "#{params[:name].upcase}%").
+        where(:created_at => search_start..search_end).distinct
     elsif params[:name]
       @tickets = Ticket.where("name like ?", "#{params[:name].upcase}%").where(active: true).or(
                 Ticket.where("name like ?", "#{params[:name].upcase}%").where("updated_at > ?", 1.day.ago))
     elsif params[:room]
-      @ticket = Ticket.joins(:details).where("room = ?", "#{params[:room]}").where(active: true).distinct.or(
+      @tickets = Ticket.joins(:details).where("room = ?", "#{params[:room]}").where(active: true).distinct.or(
         Ticket.joins(:details).where("room = ?", "#{params[:room]}").where("tickets.updated_at > ?", 1.day.ago).distinct)
     end
   end
 
-  def flatten_date_array(hash)
-    %w(1 2 3).map { |e| hash["date(#{e}i)"].to_i }
+  def flatten_date_array(hash, type)
+    %w(1 2 3).map { |e| hash["#{type}(#{e}i)"].to_i }
   end
 end
